58115595fc19e13394a904acffe0bd6e7b164b8e
NeurohiveVlad <95908239+NeurohiveVlad@users.noreply.github.com>
2023-04-21 10:13:51 +0300

    Update Add base

---
+++ iOS-Arduino-control-app/iOS-Arduino-control-app/iOS_Arduino_control_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Arduino_control_appApp.swift
+//  iOS-Arduino-control-app
+//
+//  Created by Vladislav Balashov on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Arduino_control_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
